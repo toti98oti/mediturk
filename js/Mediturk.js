38fe74962401c776457439c0765f7801c4c1afc0
allaouiOtmane <otmani.98far@gmail.com>
2019-05-03 18:43:07 +0000

    commit intalisation

---
+++ js/Mediturk.js
@@ -0,0 +1,39 @@
+const hamburgerButton = document.querySelector('.hamburger');
+const naveactive = document.querySelector('.nave-content');
+
+hamburgerButton.onclick = () => {
+  hamburgerButton.classList.toggle('activve');
+  naveactive.classList.toggle('nave-active');
+}
+
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+var acc = document.getElementsByClassName("accordions");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
